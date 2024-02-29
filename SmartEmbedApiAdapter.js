d82f0783a0c8482efa45b2748765ba4abf9227c4
Brian Petro <brian@plexm.com>
2024-02-29 15:08:00 -0500

    Refactor SmartEmbedApiAdapter constructor to handle http_request_adapter

--- SmartEmbedApiAdapter.js
+++ SmartEmbedApiAdapter.js
@@ -1,9 +1,16 @@
 const { SmartEmbed } = require("./SmartEmbed");
 
 class SmartEmbedApiAdapter extends SmartEmbed {
-  constructor(model_config_key, http_request_adapter, api_key, opts = {}) {
+  constructor(model_config_key, http_request_adapter=null, api_key, opts = {}) {
     super(model_config_key);
-    this.http_request_adapter = http_request_adapter;
+    // this.http_request_adapter = http_request_adapter;
+    // this.http_request_adapter = (http_request_adapter || fetch).bind(window);
+    if(http_request_adapter) this.http_request_adapter = http_request_adapter;
+    else {
+      if(typeof fetch === "undefined") throw new Error("http_request_adapter is required when fetch is not available");
+      this.http_request_adapter = fetch.bind(window);
+      opts.url_first = true;
+    }
     this.api_key = api_key;
     this.opts = opts;
   }
